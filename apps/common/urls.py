38d605a620f2eb4b6ca5e6c229b2b84c1cb905ab
yaochenfeng <chenfengyao@foxmail.com>
2023-06-09 02:16:46 +0800

    feat: 用户信息拓展

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
+    path(f'{settings.STATIC_URL.strip("/")}/<path:path>', serve, {"document_root": settings.STATIC_ROOT}),
+    path(f'{settings.MEDIA_URL.strip("/")}/<path:path>', serve, {"document_root": settings.MEDIA_ROOT}),
     path('', include('admin_volt.urls')),
 ]
